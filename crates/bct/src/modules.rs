b91a65e0dcb13ed1a1aca7d3f13ebfbb6768f671
Brian Anderson <andersrb@gmail.com>
2025-02-13 23:07:46 -0700

    .

--- crates/bct/src/modules.rs
+++ crates/bct/src/modules.rs
@@ -1,12 +1,23 @@
 use rmx::prelude::*;
 
 use rmx::blake3;
+use rmx::alloc::collections::BTreeMap;
 
 use crate::input::Source;
 
+#[derive(Copy, Clone, Debug, salsa::Update)]
+#[derive(Eq, PartialEq, Ord, PartialOrd)]
+pub struct SourceHashId([u8; 32]);
+
+#[salsa::input]
+pub struct ModuleMap {
+    #[return_ref]
+    pub import_to_hash: BTreeMap<String, SourceHashId>,
+}
+
 #[salsa::tracked]
 pub struct SourceHash<'db> {
-    hash: [u8; 32],
+    hash: SourceHashId,
 }
 
 #[salsa::tracked]
@@ -15,5 +26,5 @@ pub fn source_hash<'db>(
     source: Source,
 ) -> SourceHash<'db> {
     let hash = blake3::hash(source.text(db).as_bytes()).into();
-    SourceHash::new(db, hash)
+    SourceHash::new(db, SourceHashId(hash))
 }
